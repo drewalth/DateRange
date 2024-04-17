bce3cf200be08e344b19dd8faf1f1f5a66b93545
drewalth <16584198+drewalth@users.noreply.github.com>
2024-04-17 13:29:39 -0600

    build: package platforms updated

--- Package.swift
+++ Package.swift
@@ -4,20 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "DateRange",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "DateRange",
-            targets: ["DateRange"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "DateRange"),
-        .testTarget(
-            name: "DateRangeTests",
-            dependencies: ["DateRange"]),
-    ]
-)
+  name: "DateRange",
+  platforms: [
+    .iOS(.v15),
+    .macOS(.v10_15),
+    .watchOS(.v6),
+    .tvOS(.v13),
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "DateRange",
+      targets: ["DateRange"]),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "DateRange"),
+    .testTarget(
+      name: "DateRangeTests",
+      dependencies: ["DateRange"]),
+  ])
